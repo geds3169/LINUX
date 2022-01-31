6669b5e767b5210d0796834752d4199e0f9c8e0d
guilhem Schlosser <guilhemETkarine@hotmail.fr>
2022-01-31 17:05:08 +0100

    Test

--- Project.sh
+++ Project.sh
@@ -40,15 +40,18 @@ echo "Installation d'outils"
 sudo apt install net-tools -y -q
 sudo apt install locate -y -q
 sudo updatedb
+
 echo ""
-echo "On passe dans l'install d'Apache2"
+echo "installation d'Apache2"
+
 # Determine si le service apache est installé et s'il fonctionne, si le service est actif au démarrage
 if [[ "$(dpkg --get-selections | grep apache2 | grep -v "apache2-" )" =~ "install" ]]
 then
 		echo "Apache2 est installé"
 		# Determine si le seveur web est fonctionnel.
-		if [ "${APACHE2_STATUS}" = "${FLAG_STATUS}" ]; then
+		if [ ! "${APACHE2_STATUS}" = "${FLAG_STATUS}" ]; then
 			echo "Apache2 est démarré"
+
 		else
 			echo "Apache 2 n'est pas démarré"
 			echo "Voulez-vous démarrer Apache2 [y/n] ? "
@@ -85,7 +88,7 @@ echo "On vérifie si tar existe dans le répertoire courant et on créé le rép
 echo ""
 cd /tmp/
 # check si l'archive tar de la solution existe dans /tmp/
-if [ -f /tmp/$file ]; then
+if [ -f /tmp/"${file}" ]; then
 	echo "L'archive existe déja et pourra être décompréssé dans ${dir}"
 else
 	echo "Téléchargement de l'archive depuis le dépot officiel https://download.owncloud.org "
@@ -104,7 +107,7 @@ if [ -d "$dir" ]
 then
 	if [ "$(ls -A $dir)" ]; then
 		echo "$dir n'est pas vide"
-		echo "Voulez vous supprimer le contenu et décompresser l'archive dans le répertoire $dir ?"
+		echo "Voulez vous supprimer le contenu et décompresser l'archive dans le répertoire $dir [y/n] ?"
 		read DelInstall
 		if [ "${DelInstall}" == "yes" ] || [ "${DelInstall}" == "y" ]; then
 			sudo rm -r $dir/*
@@ -114,7 +117,7 @@ then
 		fi
 	else
 		echo "$dir existe cependant il est vide et peut donc être utilisé"
-		echo "Voulez vous décompresser l'archive dans le répertoire $dir ?"
+		echo "Voulez vous décompresser l'archive dans le répertoire $dir [y/n] ?"
 		read Install
 		if [ "${Install}" == "yes" ] || [ "${Install}" == "y" ]; then
 			sudo tar xvf owncloud-complete-20211220.tar.bz2 --strip-components=1 -C $dir
@@ -124,7 +127,7 @@ then
 	fi
 else
 	echo "Le répertoire $dir n'existe pas."
-	echo "Voulez vous créer le répertoire $dir ?"
+	echo "Voulez vous créer le répertoire $dir [y/n] ?"
 	read q
 	if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
 		sudo mkdir $dir
@@ -134,7 +137,7 @@ else
 fi
 
 # Nettoyage des répertoire utilisés durant l'execution du script
-echo "Voulez-vous nettoyez le fichier téléchargés ?"
+echo "Voulez-vous nettoyez le fichier téléchargés [y/n] ?"
 read Clean
 if [ "${Clean}" == "yes" ] || [ "${Clean}" == "y" ]; then
 rm -R /tmp/owncloud-complete-*
