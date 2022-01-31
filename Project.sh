253bce0afc0931e90c3103747eae3c91b24fabfd
guilhem Schlosser <guilhemETkarine@hotmail.fr>
2022-01-31 09:50:46 +0100

    up

--- Project.sh
+++ Project.sh
@@ -34,20 +34,21 @@ PHP_STATUS="$(systemctl status php8.1-fpm)"
 
 # Flags
 START_SCRIPT_DEBUG="true"
-FLAG_STATUS="active"
+FLAG_ACTIVE="active"
+FLAG_ENABLED="enabled"
 
 #######################################
 # Mise à jour du système et des packets
 #######################################
 echo "Mise à jour du système et des sources logicielles"
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
 echo ""
 echo "Installation d'outils dédié au réseau"
-apt-get install net-tools -y
+sudo apt-get install net-tools -y
 echo "le systeme est a présent à jour"
 echo ""
 echo "Installation d'outil de recherche dans le systeme (locate)"
-apt-get install locate -y
+sudo apt-get install locate -y
 sudo updatedb
 sleep 2
 
@@ -70,10 +71,10 @@ else
 	read installApache2
 	if [ "${installApache2}" == "yes" ] || [ "${installApache2}" == "y" ];
 	then
-		apt install apache2 -y
+		sudo apt install apache2 -y -q
 		echo "Activation du service"
-		systemctl enable apache2
-		echo -e "Systemctl status apache2"
+		sudo systemctl enable apache2 -q | grep "apache2" ${FLAG_ENABLED} && echo "$FLAG_ENABLED"
+		sudo Systemctl status apache2 -q |grep "apache2" ${FLAG_ACTIVE} && echo "$FLAG_ACTIVE"
 	fi
 fi
 sleep 5
@@ -87,10 +88,9 @@ else
 	read installMariaDB
 	if [ "${installMariaDB}" == "yes" ] || [ "${installMariaDB}" == "y" ];
 	then
-		apt install mariadb-server -y
+		sudo apt install mariadb-server -y
 		echo "Activation du service"
-		systemctl enable mariadb
-		echo -e "Systemctl status mariadb"
+		sudo Systemctl status apache2 -q |grep "mariadb" ${FLAG_ACTIVE} && echo "$FLAG_ACTIVE"
 	fi
 fi
 sleep 5
@@ -98,7 +98,7 @@ sleep 5
 #############################################
 # Determine si Apache2 est fonctionnel.
 #############################################
-if [ "${APACHE2_STATUS}" = "${FLAG_STATUS}" ] 
+if [ "${APACHE2_STATUS}" = "${FLAG_ACTIVE}" ] 
 then
 	echo "Apache2 est démarré"
 else
@@ -107,8 +107,8 @@ else
 	read activeApache2
 	if [ "${activeApache2}" == "yes" ] || [ "${activeApache2}" == "y" ];
 	then
-		systemctl start apache2
-		echo -e "systemctl status apache2"
+	sudo Systemctl status apache2 -q |grep "mariadb" ${FLAG_ACTIVE} && echo "$FLAG_ACTIVE"
+	sudo systemctl enable apache2 -q | grep "mariadb" ${FLAG_ENABLED} && echo "$FLAG_ENABLED"
 	fi
 fi
 sleep 5
@@ -116,7 +116,7 @@ sleep 5
 #############################################
 # Determine si Apache2 est fonctionnel.
 #############################################
-if [ "${MARIADB_STATUS}" = "${FLAG_STATUS}" ] 
+if [ "${MARIADB_STATUS}" = "${FLAG_ACTIVE}" ] 
 then
 	echo "MariaDB est démarré"
 else
@@ -125,8 +125,8 @@ else
 	read activeApache2
 	if [ "${activeApache2}" == "yes" ] || [ "${activeApache2}" == "y" ];
 	then
-		systemctl start MariaDB
-		echo -e "systemctl status MariaDB"
+		systemctl start MariaDB |grep "mariadb" ${FLAG_ACTIVE} && echo "$FLAG_ACTIVE"
+		sudo systemctl enable apache2 -q | grep "mariadb" ${FLAG_ENABLED} && echo "$FLAG_ENABLED"
 	fi
 fi
 sleep 5
@@ -143,7 +143,7 @@ else
 	read enableApache2
 	if [ "${enableApache2}" == "yes" ] || [ "${enableApache2}" == "y" ];
 	then
-		systemctl enable apache2
+		sudo systemctl enable apache2 -q | grep 
 	fi
 fi
 sleep 5
@@ -168,6 +168,7 @@ sleep 5
 #########################################################
 # Determine si PHP 8 est installé
 #########################################################
+:'
 if [ $(dpkg -l | grep php) -eq 0 ] || [ ${PHPVERSION} >= 7.0 ] ;
 then
 	# Installation des packets
@@ -191,7 +192,19 @@ then
 	$PHP_STATUS
 	sleep 5
 fi
+'
+
+echo ""
+
+sleep 1.0
+# Retour d'information sur le processus Apache2, MariaDB et ports utilisés
+pgrep -lf apache2
+pgrep -lf mariadb
+echo ""
+echo "Protocole et ports d'écoute sont :"
+netstat -pat | grep apache2|grep mariadb
+echo ""
 
 # !!!!!!!!!!!!!! voir à implémenter la désactivation des versions antérieures e.g: sudo a2dismod php5 / sudo a2enmod php8.1 / systemctl restart apache2 et voir a combiner avec la mise en fonction des versions installé localement mais non actives
 # locate -i -e  "php.*"  puis sudo update-alternatives --config php et enfin sudo update-alternatives --set php /usr/bin/php8 si existant
-https://stackoverflow.com/questions/42619312/switch-php-versions-on-commandline-ubuntu-16-04
+#https://stackoverflow.com/questions/42619312/switch-php-versions-on-commandline-ubuntu-16-04
