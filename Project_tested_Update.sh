ba5d2f21260751655c8fb7c84f4620d681ae12d5
SCHLOSSER Guilhem <guilhemETkarine@hotmail.fr>
2022-02-02 16:52:16 +0100

    Update Project_tested_Update.sh
    Script remanié (en quasi totalité)
    -Ajout de fonctions
    -Ajout d'un menu friendly
    -Ajout de sorties de log

--- Project_tested_Update.sh
+++ Project_tested_Update.sh
@@ -7,6 +7,15 @@
 #
 #Code permettant la mise en place d'une solution, test du script pas à pas
 #
+##########################################################################
+######################################
+# Vérification des droits d'execution
+######################################
+if [ "$(whoami)" != "root" ]; then
+	echo "Les privilèges Root sont requis pour exécuter ce script, essayez de l'exécuter avec sudo..."
+	exit 2
+fi
+
 #########################
 # Variables
 #########################
@@ -24,69 +33,130 @@ FLAG_ACTIVE="active"
 FLAG_ENABLED="enabled"
 #
 file="owncloud-complete-latest.tar.bz2"
-mytitle="Installation de la solution cloud privé ownCloud"
+mytitle="Installation d'une solution cloud privé"
+
 clear
-######################################
-# Vérification des droits d'execution
-######################################
-if [ "$(whoami)" != "root" ]; then
-	echo "Les privilèges Root sont requis pour exécuter ce script, essayez de l'exécuter avec sudo..."
-	exit 2
-fi
 
-#####################################
-# Tools
-#####################################
-function tools(){
-echo "Installation d'outils réseau"
-echo ""
-echo -e "Net-tools: qui contient des outils: arp, ifconfig, netstat, rarp, nameif et route \ndnsutils: implémente un serveur de noms de domaine"
-sudo apt-get install net-tools -y >> test.log
-sudo apt-get  install dnsutils -y >> test.log
-sudo apt-get  install ifupdown2 -y >> test.log
-echo ""
-echo "Installation d'outil dédié à la recherche de fichiers"
-echo ""
-echo "locate: permet de rechercher un fichier: locate fichier.txt | less | more"
-echo "Pensez à passer la commande: updatedb avant la recherche "
-echo ""
-sudo apt-get  install locate -y >> test.log
-echo ""
-sudo updatedb
-echo ""
-# /usr/bin/find: '/run/user/1000/gvfs': Permission denied --> erreur normale
-echo -e "Il ne s'agit pas d'une erreur updatedb s'est terminé avec succés, \nmais n'a pas pu lire cet emplacement voir: \nhttps://dev.getsol.us/T5545"
-echo "Installation d'un outil de visualisation de l'arborescence,des répertoire, dossier ..."
-echo "visualiser une arborescence avec tree: tree -a  ou -f ou encore -dfp"
-echo ""
-sudo apt-get install tree -y >> test.log
-}
-
-######################################
-# Titre
-######################################
-mytitle="Installation simplifié d'un cloud privé: Solution ownCloud"
-echo -e '\033k'$mytitle'\033\\'
-sleep 5
-echo ""
 ######################################
 # Mise à jour systeme et packets
 ######################################
-echo "Mise à jour du systeme et des packets"
+function update(){
+echo "Souhaitez-vous procéder à la mise à jour du systeme et des packets [y/n] ?"
+read q
+if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+	echo -e "La mise à jour va débuter \n"
+	sudo apt-get update && apt-get upgrade -y -qq >> update.log
+	echo -e "\nMise à jour terminé, un fichier de log nommé update.log se trouve dans le répertoire courant.\n"
+fi
+
+sleep 2
+clear
+}
+
+#####################################
+# fonction Tools
+#####################################
+function tools(){
+
+#Réseau
+tools1="net-tools"
+tools2="dnsutils"
+tools3="ifupdown2"
+
+#Indexation/Recherche
+tools4="locate"
+tools5="tree"
+
+# Determine si le paquet est installé / installe
+echo -e "Outils réseau\n"
+if [[ "$(dpkg --get-selections | grep $tools1)" =~ "install" ]]; then
+                echo "$tools1 est déjà présent"
+else
+        echo "$tools1 n'est pas installé"
+        echo "Souhaitez-vous procéder à son installation [y/n] ? "
+        read q
+        if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+                sudo apt-get install $tools1 -y >> tools.log
+		echo -e "$tools1 a été installé, il contient des outils tel que:\n arp, ifconfig, netstat, rarp, nameif et route"
+		echo -e "\n Un fichier tools.log a été créé dans le répertoire courant.\n"
+        fi
+fi
+
+
+if [[ "$(dpkg --get-selections | grep $tools2)" =~ "install" ]]; then
+                echo "$tools2 est déjà présent"
+else
+        echo "$tools2 n'est pas installé"
+        echo "Souhaitez-vous procéder à son installation [y/n] ? "
+        read q
+        if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+                sudo apt-get install $tools1 -y >> tools.log
+                echo -e "$tools1 a été installé, il contient des outils permettant d'implémenter un serveur de nom de domaine."
+                echo -e "\n Un fichier tools.log a été créé dans le répertoire courant.\n"
+        fi
+
+fi
+
+echo -e "Outils réseau\n"
+if [[ "$(dpkg --get-selections | grep $tools3)" =~ "install" ]]; then
+                echo "$tools3 est déjà présent"
+else
+        echo "$tools3 n'est pas installé"
+        echo "Souhaitez-vous procéder à son installation [y/n] ? "
+        read q
+        if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+                sudo apt-get install $tools3 -y >> tools.log
+		echo -e "$tools3 a été installé, il contient des outils de gestion des interfaces réseau"
+		echo -e "\n Un fichier tools.log a été créé dans le répertoire courant.\n"
+        fi
+fi
+
+
+echo -e "Outil de d'indexation/recherche de fichier\n"
+if [[ "$(dpkg --get-selections | grep $tools4)" =~ "install" ]]; then
+                echo "$tools4 est déjà présent"
+else
+        echo "$tools4 n'est pas installé"
+        echo "Souhaitez-vous procéder à son installation [y/n] ? "
+        read q
+        if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+                sudo apt-get install $tools4 -y >> tools.log
+		echo -e "$tools4 a été installé, il permet de rechercher des fichiers. \nUsage, passez au préalable la commande suivante: \n (! sudo si nécessaire !) updatedb \n Enfin utiliser la recherche avec la commande suivante: \nlocate fichier.txt | less | more \n Pensez à passer la commande: updatedb avant toutes recherche."
+		echo -e "Le retour d'erreur suivant: \n/usr/sbin/find: '/run/user/1000/gvfs': Permission denied  --> n'est pas une erreur (comportement attendu/normal) \nVoir: https://dev.getsol.us/T5545 \nCela n'empêche nullement son utilisation."
+		echo -e "\n Un fichier tools.log a été créé dans le répertoire courant.\n"
+        fi
+fi
+
+if [[ "$(dpkg --get-selections | grep $tools5)" =~ "install" ]]; then
+                echo "$tools5 est déjà présent"
+else
+	echo "$tools5 n'est pas installé"
+	echo "Souhaitez-vous procéder à son installation [y/n] ? "
+	read q
+        if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+                sudo apt-get install $tools4 -y >> tools.log
+		echo -e "$tools5 a été installé, il permet la visualisation de l'arborescence, des répertoire, dossier ...\n"
+		echo -e "La commande est : tree -a ou tree -f ou encore tree -dfp"
+		echo -e "\n Un fichier tools.log a été créé dans le répertoire courant.\n"
+	fi
+fi
+
+echo -e "\nInstallation des outils terminé le fichier tools.log comportant les informations sur les outils, \nse trouve dans le repertoire courant se trouve dans le répertoire courant.\n"
+sleep 3
+clear
+}
+
+########################
+# fonction ownCloud
+########################
+function owncloud(){
+
+echo -e "Installation de la solution ownCloud\n"
 echo ""
-sudo apt-get update && apt-get upgrade -y -qq >> test.log
-#clear
-
-# Appel de la fonction d'installation des outils
-tools | tee -a test.log
-
-sleep 10
-
-echo "Mise en place du serveur Web"
+echo "\nMise en place du serveur Web"
 echo ""
 # Determine si le service apache est installé et s'il fonctionne, si le service est actif au démarrage
-if [[ "$(dpkg --get-selections | grep apache2 | grep -v "apache2-" )" =~ "install" ]]
-then
+if [[ "$(dpkg --get-selections | grep apache2 | grep -v "apache2-" )" =~ "install" ]]; then
 		echo "Apache2 est installé"
 		# Determine si le seveur web est fonctionnel.
 		if [ "${APACHE2_STATUS}" == "${FLAG_ACTIVE}" ]; then
@@ -124,7 +194,7 @@ fi
 
 sleep 5
 clear
-echo "Mise en place du serveur de base de données"
+echo "Mise en place du serveur de bas de base de données"
 echo ""
 sleep 5
 # Determine si le service Mariadb est installé et s'il fonctionne, si le service est actif au démarrage
@@ -168,10 +238,10 @@ elif [[ "$(dpkg --get-selections | grep mysqld)" =~ "install" ]]; then
         # Determine si le service est actif au démarrage.
         if [ "${MySQL_SERVICE}" == "${FLAG_ENABLED}" ]; then
                 echo "Le service MySQL est activé"
-        else 
+        else
                 echo "Le service MySQL n'est pas activé"
-                echo "Voulez-vous activer le service MySQL [y/n] ?" 
-                read enableMySQL 
+                echo "Voulez-vous activer le service MySQL [y/n] ?"
+                read enableMySQL
                 if [ "${enableMySQL}" == "yes" ] || [ "${enableMySQL}" == "y" ]; then 
                         sudo systemctl enable mysqld.service
                 fi
@@ -224,7 +294,7 @@ sleep 5
 clear
 echo "Création du répertoire, installation de la solution, mise en place de la  configuration"
 echo ""
-sleep 5 
+sleep 5
 # Cherche si le répertoire et existant ou vide
 if [ -d "$dir" ]
 then
@@ -270,5 +340,88 @@ if [ "${Clean}" == "yes" ] || [ "${Clean}" == "y" ]; then
 	rm -R /tmp/owncloud-complete-*
 	sudo ls /tmp/
 fi
+}
+
+#####################################
+# fonction menu
+####################################
+show_menu(){
+
+mytitle="Installation d'une solution cloud"
+echo -e "${title} ##################################### ${normal}\n"
+echo -e "${title} # ${mytitle} #${normal}\n"
+echo -e "${title} ##################################### ${normal}\n\v"
+
+	title=`echo "\033[35m"` #Purple
+	normal=`echo "\033[m"`
+	menu=`echo "\033[36m"` #Cyan
+	number=`echo "\033[33m"` #Yellow
+	validation=`echo "\033[32m"` #Green
+	bgred=`echo "\033[41m"`
+	fgred=`echo "\033[31m"`
+
+	# Optionnal colors
+	# `echo "\033[37m"` white
+	# `echo "\033[30m"` Black
+
+echo -e "\n${menu}*********************************************${normal}\n"
+echo -e "${menu}**${number} 1)${menu} Mise à jour du systeme et des logiciels (Recommandé) ${normal}\n"
+echo -e "${menu}**${number} 2)${menu} Installation des outils (optionnels) ${normal}\n"
+echo -e "${menu}**${number} 3)${menu} Installation de ownCloud ${normal}\n"
+echo -e "${menu}**${number} 4)${menu} Installation de Nextcloud ${normal}\n"
+echo -e "${menu}*********************************************${normal}\n"
+echo -e "Sélectionnez une option pressez ${fgred}x pour quitter. ${normal}"
+read opt
+}
+
+option_picked(){
+msgcolor=`echo "\033[01;31m"` # bold red
+normal=`echo "\033[00;00m"` # normal white
+message=${@:-"${normal}Error: No message passed"}
+echo -e "${msgcolor}${message}${normal}\n"
+}
+
+clear
+show_menu
+while [ $opt != '' ]
+    do
+    if [ $opt = '' ]; then
+      exit;
+    else
+      case $opt in
+	1) clear;
+            option_picked "Mise à jour du systeme et des logiciels";
+            update | tee -a tools.log; #appel de la function
+            show_menu;
+	;;
+        2) clear;
+            option_picked "Tools";
+            tools; #appel de la function
+            show_menu;
+        ;;
+        3) clear;
+            option_picked "ownCloud";
+            owncloud; #appel de la function
+            show_menu;
+        ;;
+        4) clear;
+            option_picked "Nextcloud";
+            nextcloud; #appel de la function
+            show_menu;
+        ;;
+        x)exit;
+        ;;
+        \n)exit;
+        ;;
+        *)clear;
+            option_picked "Sélectionnez une option dans le menu";
+            show_menu;
+        ;;
+      esac
+    fi
+done
+########################################################################
+
+echo "Bye bye !"
 
 exit 0
