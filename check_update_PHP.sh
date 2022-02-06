bf2301a0ae77344b84932d65222e69e01b1d54f5
guilhem Schlosser <guilhemETkarine@hotmail.fr>
2022-02-06 21:13:38 +0100

    Correction, check version local

--- check_update_PHP.sh
+++ check_update_PHP.sh
@@ -28,10 +28,11 @@ if [[  "$(dpkg --get-selections | grep "php*")" =~ "install" ]]; then
 	if [ $MAJOR_CURRENTVERS -ge $MAJOR_REQ ] && [ $MINOR_CURRENTVERS -ge $MINOR_REQ ]; then
 		echo -e "\nLa version actuelle correspond aux attentes de la solution"
 	else
-		echo -e "\nLa version actuelle ne correspond pas aux attentes de la solution"
+		echo -e "\nLa version actuelle ne correspond pas aux attentes de la solution \nelle nécessite la version $REQUIRED "
 		# Vérification de l'existance de version supérieure PHP sur la distribution
-		if [[ -d /etc/php ]]; then
+		if [[ -d /etc/php && "$(echo find /etc/php -mindepth 1 -maxdepth 1 -type d | wc -l)" -gt 1 ]]; then
 			echo -e "\nVoici a présent les versions de PHP disponible localement sur votre distribution:\n"
+			cd /etc/php
 			dir
 			echo -e "\nVoulez-vous changer de version [y/n] ?"
 			read q
@@ -53,11 +54,11 @@ if [[  "$(dpkg --get-selections | grep "php*")" =~ "install" ]]; then
 			fi
 		else
 			echo -e "\nIl n'existe pas d'autre version PHP en local\n"
-			echo "Souhaitez-vous vérifier s'il existe une version plus récente sur les dépôts Debian [y/n] ?
+			echo "Souhaitez-vous vérifier s'il existe une version plus récente sur les dépôts Debian [y/n] ?"
 			if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
-				"La version la plus récente est :"
+				echo "La version la plus récente est :"
 				$AVAILABLE
-				echo "Souhaitez-vous installer la nouvelle version [y/n] ?
+				echo "Souhaitez-vous installer la nouvelle version [y/n] ?"
 				if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
 					sudo apt-get install php$AVAILABLE -y -q >> PHP.log
 					echo -e "\nLe paquet a été installé, le fichier PHP.log a été mis à jour, \nil se trouve dans le répertoire courant\n"
