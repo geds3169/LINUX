721d0f31052adace062bc6464351ed64dab906a7
guilhem Schlosser <guilhemETkarine@hotmail.fr>
2022-02-06 18:06:19 +0100

    Add comment

--- check_update_PHP.sh
+++ check_update_PHP.sh
@@ -39,7 +39,7 @@ fi
 #sudo apt-get install apt-show-versions - y -q >> tools.log
 #echo -e "\nLe paquet a été installé, le fichier tool.log a été mis à jour, \nil se trouve dans le répertoire courant\n"
 #fi
-}
+#}
 
 
 ############################
