e729262cea33f2d1e47a4b4661fbf82f8a87c6a5
guilhem Schlosser <guilhemETkarine@hotmail.fr>
2022-02-06 14:56:04 +0100

    Modification de l'imbrication

--- check_update_PHP.sh
+++ check_update_PHP.sh
@@ -13,53 +13,60 @@ PACKAGE_NAME="php"
 List_PHP_V="$(cd /etc/php && dir)"
 
 
-echo -e "Installation du paquet bc qui permet de faire des calculs (versions)\n"
-sleep 3
-sudo apt-get install bc -y -q >> tools.log
-echo -e "\nLe fichier tools.log a été créé/mis à jour, il se trouve dans le répertoire courant."
-clear
-echo -e "\nVérification de la présence de PHP et des modules\n"
-# Determine si PHP est installé sur le système, la version minimale requise
-if [[  "$(dpkg --get-selections | grep "php*")" =~ "install" ]]; then
-	echo "PHP est déjà présent sur le systeme, la version est  $CURRENT_VERSION"
-	# check si la version minimale requise est installé et utilisé
-	if [ $(echo " $CURRENT_VERSION >= $MINI_VERSION" | bc) -eq 1 ]; then
-        	echo "La version actuelle correspond aux attentes de la solution"
+echo -e "L' installation du paquet bc est absolument nécessaire afin de peremettre la comparaison des versions\n"
+sleep 1
+echo -e "\nSouaitez-vous installer le paquet ?"
+read q
+if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+	sudo apt-get install bc -y -q >> tools.log
+	echo -e "\nLe fichier tools.log a été créé/mis à jour, il se trouve dans le répertoire courant."
+	sleep 2
+	clear
+
+	echo -e "\nVérification de la présence de PHP et des modules\t"
+	# Determine si PHP est installé sur le système, la version minimale requise
+	# Determine si PHP est installé sur le système, la version minimale requise
+	if [[  "$(dpkg --get-selections | grep "php*")" =~ "install" ]]; then
+		echo "PHP est déjà présent sur le systeme, la version est  $CURRENT_VERSION"
+		
+		# check si la version minimale requise est installé et utilisé
+		if [ $(echo " $CURRENT_VERSION >= $MINI_VERSION" | bc) -eq 1 ]; then
+			echo "La version actuelle correspond aux attentes de la solution"
 			echo -e "\nVoulez-vous vérifier s'il existe une version plus récente ?"
 			read q
 			if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
-			if [[  "$(dpkg --get-selections | grep "apt-show-versions")" =~ "install" ]]; then
-				sudo apt list --upgradable -V
-			else
-				echo -e "\nle paquet apt-show-versions n'est pas installé, il est requis pour la vérification des versions"
-				echo -e "Souhaitez-vous installer le paquet ?"
-				read -q
-				if  [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
-					sudo apt-get install apt-show-versions
+				if [[  "$(dpkg --get-selections | grep "apt-show-versions")" =~ "install" ]]; then
+					echo -e "\nVoici la liste des versions disponible pour votre distribution \n" 
+					sudo apt list --upgradable -V
 				else
-					echo "Il est impossible de tester si une version plus récente existe sans ce paquet"
+					echo -e "\nle paquet apt-show-versions n'est pas installé, il est requis pour la vérification des versions"
+					echo -e "Souhaitez-vous installer le paquet ?"
+					read -q
+					if  [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+						sudo apt-get install apt-show-versions - y -q >> tools.log
+						echo -e "\nLe paquet a été installé, le fichier tool.log a été mis à jour, \nil se trouve dans le répertoire courant\n"
+						echo -e "\nVoici la liste des versions disponible pour votre distribution \n"
+						sudo apt list --upgradable -V
+					else
+						echo "Vous avez choisi de refuser l'installation, le script va donc poursuite."
+					fi
 				fi
+			else
+				echo -e "\nCe choix et de votre responsabilité, il est recommandé de consultez les CVE"
+				echo "http://www.cvedetails.com/product/128/PHP-PHP.html?vendor_id=74"
 			fi
-	else
-		echo -e "\nCe choix et de votre responsabilité, il est recommandé de consultez les CVE"
-		echo "http://www.cvedetails.com/product/128/PHP-PHP.html?vendor_id=74"
-	fi
-else
-    echo "La version actuelle $CURRENT_VERSION de PHP ne correspond pas au minimum requis"
-	echo -e "Voulez-vous installer une version plus récente de PHP ?"
-	read q
-	if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
-		sudo apt-get install php -y -q >> PHP.log
-		echo "La mise à jour de PHP a été faite, un fichier nommé PHP.log a été créé/mis à jour \nil se trouve dans le répertoire courant"
-		echo -e "Voici a présent les version de PHP disponible sur votre distribution:\n"
+		fi
+		
+		# check des versions disponible localement
+		echo -e "\nVoici a présent les version de PHP disponible localement sur votre distribution:\n"
 		List_PHP_V
-		echo -e "\nVoulez-vous changer la version ?"
+		echo -e "\nVoulez-vous changer de version ?"
 		read q
 		if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
 			echo -e "\nRenseignez le numéro de version souhaitez (retourné précédemment): "
 			read new_php_version
 			echo -e "\nDésactivation de PHP $CURRENT_VERSION"
-			sudo /usr/bin/a2dismod $CURRENT_VERSION"
+			sudo /usr/bin/a2dismod $CURRENT_VERSION
 			echo -e "\nActivation de la version $new_php_version"
 			sudo /usr/bin/a2enmod $new_php_version
 			echo -e "\nLe redémarrage du serveur Apche2 est nécessaire, voulez-vous procéder ?"
@@ -72,7 +79,20 @@ else
 		else
 			echo "La version de PHP n'a pas été vous utilisé la version modifié $CURRENT_VERSION \nL'installation va poursuivre  mais le succès de l'installation n'est pas garanti\n"
 		fi
+		
+	else
+		echo -e "\nPHP n'est pas présent sur votre distribution.\n"
+		echo -e "Souhaitez-vous installer PHP ?"
+		read q
+		if [ "${q}" == "yes" ] || [ "${q}" == "y" ]; then
+			sudo apt-get install php -y -q >> PHP.log
+			echo "La mise à jour de PHP a été faite, un fichier nommé PHP.log a été créé/mis à jour \nil se trouve dans le répertoire courant"
+		fi
 	fi
+else
+	echo -e "\nVotre refus d'installer le paquet ne permet pas de confirmer la présence de PHP et s''il respecte la version requise\n"
+	echo "Le bon fonctionnement de l'installation dès lors, ne peut être garanti."
 fi
+
 
 exit 0
